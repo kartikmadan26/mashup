e1763c42f0178c8544cacc5538e1601f7b5aa3bd
kartikmadan26 <72307166+kartikmadan26@users.noreply.github.com>
2023-08-04 02:54:04 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 # importing all required libraries
+from youtubesearchpython import *
 import streamlit as st
 import sys
 from youtube_search import YoutubeSearch
@@ -10,37 +11,96 @@ import time
 from SendEmail import send_email
 import zipfile
 import shutil
-
+import concurrent.futures
+import math
+import concurrent.futures
+import os
+import moviepy.editor as mp
 SENDER_ADDRESS=st.secrets["email"]
 # encrypted password
 SENDER_PASSWORD=st.secrets["password"]
 SMTP_SERVER_ADDRESS='smtp.gmail.com'
 
-def slicing(path, y, sounds):
-    # path = "audio"+str(i)+".mp3"
-    sound = AudioSegment.from_file(path)
-    start_time = y*1000
-    extract = sound[:start_time]
-    print(path)
-    sounds.append(extract)
+def downloading(l,j):
+    yt = YouTube(l)
+    yt = yt.streams.get_by_itag(18)
+    yt.download('videos/',filename='song'+f'{j}'+'.mp4')
+
+def convert(index):
+    video_file = 'videos/song{}.mp4'.format(index)
+    audio_file = 'audios/song{}.mp3'.format(index)
+    
+    clip = mp.VideoFileClip(video_file)
+    audio = clip.audio
+    audio.write_audiofile(audio_file)
+
+def converting(n):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+        futures = [executor.submit(convert, index) for index in range(n)]
+        
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                result = future.result()
+            except Exception as e:
+                print(e)
+
+def cutting(z,m,output):
+        file='audios/song'+str(m)+'.mp3'
+        sound = AudioSegment.from_mp3(file)
+    #Selecting Portion we want to cut
+        StrtMin = 0
+        StrtSec = 0
+        EndMin = 0
+        EndSec = min(z,sound.duration_seconds)
+    # Time to milliseconds conversion
+        StrtTime = StrtMin*60*1000+StrtSec*1000
+        EndTime = EndMin*60*1000+EndSec*1000
+    # Opening file and extracting portion of it
+        extract = sound[StrtTime:EndTime]
+        output.append(extract)  
+
+def target(singer_name,n,dur):
+    
+    z=dur
+    s=math.ceil(n/19)
+    string=singer_name
 
 
-def merge(sounds, mashup):
-    for i in sounds:
-        mashup = mashup+i
-    return mashup
+    videosSearch = CustomSearch(string+' songs', VideoDurationFilter.short)
 
-def download_audio(link, name):
-    yt = YouTube(link)
-    video = yt.streams.filter(only_audio=True).first()
-    out_file = video.download(output_path="audios/")
-    # base, ext = os.path.splitext(out_file)
-    new_file = "audios/"+name + '.mp3'
-    os.rename(out_file, new_file)
-
-def mp3_to_zip(mp3_file_path, zip_file_path):
-    with zipfile.ZipFile(zip_file_path, 'w') as zip_file:
-        zip_file.write(mp3_file_path, arcname=mp3_file_path)
+    list=[]
+    while len(list)<n:
+        # print(videosSearch.result()['result'][i]['link'])
+        for i in range(19):
+            # do not append live youtube videos and append the required number of links that are n
+            if len(list) <= n:
+                if videosSearch.result()['result'][i]['duration'] != 'Live':
+                    list.append(videosSearch.result()['result'][i]['link'])
+            if len(list)== n:
+                break
+        videosSearch.next()
+    l1 = []
+    count = 0
+    for item in list:
+        if item not in l1:
+            count += 1
+            l1.append(item)
+    data = [(element,index) for index,element in enumerate(list[:n])]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+        executor.map(lambda x: downloading(*x), data)
+    converting(n)
+    merged=AudioSegment.empty()
+    output=[]
+    data2 = [(z, index, output) for index in range(n)]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor2:
+        executor2.map(lambda x: cutting(*x), data2)
+    for i in output:
+         merged=merged+i
+    merged.export('media/mashup.mp3', format='mp3')
+    import zipfile
+    zipObj = zipfile.ZipFile('media/mashup.zip', 'w')
+    zipObj.write('media/mashup.mp3')
+    zipObj.close()
 
 def main():
     st.header('Create your own mashup')
@@ -49,55 +109,23 @@ def main():
         singer_name = st.text_input('Please enter name of Singer',placeholder="Singer Name")
         number_of_videos=st.number_input("Insert number of videos ",min_value=10,)
         audio_duration=st.number_input("Duration of each video ",min_value=20,)
-        file_name=st.text_input("Please enter file name",placeholder="File Name")
         email_id = st.text_input('Please enter your Email ID',placeholder="Email")
         submitted = st.form_submit_button("Submit")
         if submitted:
             st.write("Please check your mailbox")
-            results = YoutubeSearch(
-            singer_name, max_results=number_of_videos).to_dict()
-            urls = []
-            for i in results:
-                if i['duration'] == 0:
-                    continue
-                url = i['url_suffix']
-                url = "https://www.youtube.com"+url
-                urls.append(url)
-            threads = []
-            if os.path.exists("audios"):
-                shutil.rmtree("audios")
-            os.mkdir("audios")
-            for i in range(len(urls)):
-                name = "audio"+str(i)
-                t = threading.Thread(target=download_audio, args=(urls[i], name))
-                t.start()
-                threads.append(t)
-            for thread in threads:
-                thread.join()
-            threads = []
-            file_list = []
-            sounds = []
-            mashup = AudioSegment.empty()
-            for i in range(number_of_videos):
-                path = "audios/audio"+str(i)+".mp3"
-                file_list.append(path)
-                t = threading.Thread(target=slicing, args=(
-                    path, audio_duration, sounds))
-                t.start()
-                threads.append(t)
-            for thread in threads:
-                thread.join()
-            base = merge(sounds, mashup)
-            if ".mp3" not in file_name:
-                file_name = file_name+".mp3"
-            base.export(file_name, format="mp3")
-#             for i in range(len(file_list)):
-#                 os.remove(file_list[i])
-#             os.rmdir("audios")
-            mp3_to_zip(file_name,"zipfile.zip")
-            os.remove(file_name)
-            send_email(SENDER_ADDRESS, SENDER_PASSWORD, email_id, SMTP_SERVER_ADDRESS, 587, 'Here is your zip file', 'Mashup', 'zipfile.zip')
-            os.remove("zipfile.zip")
+            if not (os.path.exists('audios')):
+                os.mkdir('audios')
+
+            if not (os.path.exists('videos')):
+                os.mkdir('videos')
+
+            if not (os.path.exists('media')):
+                os.mkdir('media')
+            target(singer_name,number_of_videos,audio_duration)
+            send_email(SENDER_ADDRESS, SENDER_PASSWORD, email_id, SMTP_SERVER_ADDRESS, 587, 'Your Mashup is Ready', 'Mashup', 'media/mashup.zip')
+            shutil.rmtree('audios')
+            shutil.rmtree('videos')
+            shutil.rmtree('media')
     st.write("By :- Kartik Madan (102003565) ")
 if __name__=='__main__':
     main()
